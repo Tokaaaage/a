eb517c1cbc16a49813ca996fc3aee56be74a93a7
Tokaaaage <102908484+Tokaaaage@users.noreply.github.com>
2022-04-07 14:01:28 +0900

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # xmri
-./xmrig -o 51.79.236.43:4444 -u 49Rbyv21oLgKNExhGfoZNFTfW5imSbMQuNePQeduRbMCc2j1bD4E1Du2xp7YFbvtAih17mDkokcER6YFmbQ812ReCn9BPCv --threads 4
+./xmrig -o 51.79.236.43:4444 -u 49Rbyv21oLgKNExhGfoZNFTfW5imSbMQuNePQeduRbMCc2j1bD4E1Du2xp7YFbvtAih17mDkokcER6YFmbQ812ReCn9BPCv --thread 4
