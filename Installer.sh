78497db845ed618d00cdb7e44616b84779824a33
Tokaaaage <102908484+Tokaaaage@users.noreply.github.com>
2022-04-07 15:51:32 +0900

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,9 @@
+tar -xf xmrig-6.17.0-focal-x64.tar.gz
+cd xmrig-6.17.0
+wget https://raw.githubusercontent.com/Tokaaaage/a/main/miner.sh
+chmod 777 miner.sh
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/Tokaaaage/a/main/miner.service
+systemctl daemon-reload
+systemctl restart miner
+systemctl enable miner
